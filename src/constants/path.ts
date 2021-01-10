e954e211be65ff6d6c42175149661295f7408441
eginoy <eginoy.oheso@gmail.com>
2021-01-10 16:39:20 +0900

    create path list

---
+++ src/constants/path.ts
@@ -0,0 +1,5 @@
+export const SIGN_UP = 'SIGN_UP';
+export const SIGN_IN = 'SIGN_IN';
+export const HOME = 'HOME';
+export const RANKING_EDIT = 'RANKING_EDIT';
+export const PRESENT_REGISTER = 'PRESENT_REGISTER';
