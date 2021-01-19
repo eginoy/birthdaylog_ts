83d6d61c7295cb8f7d495ff20c1460bf3bfad1b0
eginoy <eginoy.oheso@gmail.com>
2021-01-19 23:46:45 +0900

    定数化したパスの値を変更

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -1,5 +1,5 @@
-export const SIGN_UP = 'SIGN_UP';
-export const SIGN_IN = 'SIGN_IN';
-export const HOME = 'HOME';
-export const RANKING_EDIT = 'RANKING_EDIT';
-export const GIFT_REGISTRATION = 'GIFT_REGISTRATION';
+export const SIGN_UP = '/SignUp';
+export const SIGN_IN = '/SignIn';
+export const HOME = '/Home';
+export const RANKING_EDIT = '/RankingEdit';
+export const GIFT_REGISTRATION = '/GiftRegistration';
