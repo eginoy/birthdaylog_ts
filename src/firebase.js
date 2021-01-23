57381b57ae39b12b02890b82e892941e42c30160
eginoy <eginoy.oheso@gmail.com>
2021-01-23 17:20:56 +0900

    firebaseインスタンス初期化ファイルの作成

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+}
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
