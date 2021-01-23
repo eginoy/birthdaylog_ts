bd4f38a205ad56054e5233a32ea3aae89650d876
eginoy <eginoy.oheso@gmail.com>
2021-01-23 17:26:08 +0900

    styledButtonコンポーネントの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
 import React from 'react';
-import {RecoilRoot} from 'recoil'
-import {Reset} from 'styled-reset'
-// import styled from 'styled-components'
+import { RecoilRoot } from 'recoil'
+import { Reset } from 'styled-reset'
 // import * as path from './constants/path'
-import {ThemeProvider as MaterialThemeProvider} from "@material-ui/styles"
-import {ThemeProvider as StyledThemeProvider} from 'styled-components'
+import { ThemeProvider as MaterialThemeProvider } from "@material-ui/styles"
+import { ThemeProvider as StyledThemeProvider } from 'styled-components'
 import theme from './constants/theme'
 import NavigationBar from './components/organisms/NavigationBar';
+import StyledButton from './components/atoms/StyledButton';
+import styled from 'styled-components'
 // import {Switch,Route} from 'react-router'
+
+const LogInButtonWrapper = styled.div`
+  margin-top: 20em;
+  display:flex;
+  justify-content:center;
+`
 
 function App() {
   return (
     <RecoilRoot>
-      <MaterialThemeProvider theme={theme}>
-        <StyledThemeProvider theme={theme}>
+      <StyledThemeProvider theme={theme}>
+        <MaterialThemeProvider theme={theme}>
           <React.Fragment>
             <NavigationBar />
+            <LogInButtonWrapper>
+              <div>
+                <StyledButton label={'Googleアカウントでログイン'} onClick={function () { }} />
+              </div>
+            </LogInButtonWrapper>
             <Reset />
           </React.Fragment>
-        </StyledThemeProvider>
-      </MaterialThemeProvider>
+        </MaterialThemeProvider>
+      </StyledThemeProvider>
     </RecoilRoot>
   );
 }
---
+++ src/components/atoms/StyledButton.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Button } from '@material-ui/core'
+
+interface Props{
+    label: string
+    onClick: () => void
+}
+
+const ThemeButton = styled(Button)`
+    &.styled{
+        background-color:${props => props.theme.palette.secondary.main};
+    }
+    &.styled:hover{
+        background-color: ${props => props.theme.palette.secondary.light}
+    }
+`
+
+const StyledButton: React.FC<Props> = ({label,onClick}) => {
+    return(
+        <ThemeButton className="styled" onClick={onClick}>
+            {label}
+        </ThemeButton>
+    )
+}
+
+export default StyledButton
