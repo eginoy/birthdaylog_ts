63e867dfea40ce3f858c013d5bf3e88f1923810c
eginoy <eginoy.oheso@gmail.com>
2021-01-26 23:34:30 +0900

    StyledSelectコンポーネントの作成

---
+++ src/components/molecules/StyledSelect.stories.js
@@ -0,0 +1,16 @@
+import StyledSelect from './StyledSelect'
+
+export default {
+    component: StyledSelect,
+    title:'StyledSelect'
+}
+
+const Template = args => <StyledSelect {...args}/>
+
+export const Default = Template.bind({});
+Default.args = {
+    users:[
+        { Uid: "eginoyUid", Name: "eginoy", Birthday: "2000-06-01" },
+        { Uid: "tarouUid", Name: "tarou", Birthday: "2000-10-30" }
+    ]
+}
---
+++ src/components/molecules/StyledSelect.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import { InputLabel, MenuItem } from '@material-ui/core'
+import { FormControl, Select } from '@material-ui/core'
+import { User } from '../../state/User'
+
+interface Props {
+    users: User[]
+}
+
+const Styled = styled.div`
+    .form{
+        width:100%;
+    }
+`
+
+const StyledSelect: React.FC<Props> = ({ users }) => {
+    const [selectedValue, setSelectedValue] = useState(users[0].Uid)
+    const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+        setSelectedValue(event.target.value as string);
+        console.log(selectedValue)
+    }
+    return (
+        <Styled>
+            <FormControl className='form'>
+                <InputLabel>誰へのプレゼント?</InputLabel>
+                <Select value={selectedValue} onChange={handleChange}>
+                    {users.map(user => {
+                        if (user.Uid) {
+                            return (
+                                <MenuItem
+                                    value={user.Uid}
+                                    key={user.Uid}
+                                >
+                                    {user.Name}
+                                </MenuItem>)
+                        } else {
+                            alert('ユーザー名の取得に失敗')
+                        }
+                    })}
+                </Select>
+            </FormControl>
+        </Styled>
+    )
+}
+
+export default StyledSelect
