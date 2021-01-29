2c89b4fe1112b19b9314fbe1a62369f1eb27c892
eginoy <eginoy.oheso@gmail.com>
2021-01-29 12:38:34 +0900

    不要コミット削除後のファイル復元

---
+++ src/components/pages/Logout/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.h2`
+    color:red;
+`
+export default function Logout(){
+    return (
+        <Wrapper>
+            <h2>SignUp Page</h2>
+        </Wrapper>
+    )
+}
