7a3516b6a162ac1dbe82a1a8eaed1206bdeded82
eginoy <eginoy.oheso@gmail.com>
2021-01-30 14:46:57 +0900

    BaseContainerのスタイル修正

--- src/components/atoms/BaseContainer.tsx
+++ src/components/atoms/BaseContainer.tsx
@@ -7,7 +7,7 @@ const BaseContainer = styled.div`
     border-radius:0.5em;
     box-shadow: 6px 6px 5px;
     margin: 2em auto;
-    padding:1em;
+    padding:0.5em 1em;
     background-color: ${props => props.theme.palette.primary.main};
     span{
         color:${props => props.theme.palette.text.primary};
