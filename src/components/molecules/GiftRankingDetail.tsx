34ea91bd41b443c4017064097fea5ca4c8527ea5
eginoy <eginoy.oheso@gmail.com>
2021-02-07 22:38:42 +0900

    GiftDetail型につけていたエイリアス削除

--- src/components/molecules/GiftRankingDetail.tsx
+++ src/components/molecules/GiftRankingDetail.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 import StyledLabel from '../atoms/StyledLabel'
-import {GiftRankingDetail as DetailType} from '../../types/Gift'
+import {GiftDetail} from '../../types/Gift'
 
 const Wrapper = styled.div`
     padding:0.5em;
@@ -11,7 +11,7 @@ const Wrapper = styled.div`
     }
 `
 
-const GiftRankingDetail = ({Rank,Name,URL,Comment,InsertUid}:DetailType) =>{
+const GiftRankingDetail = ({Rank,Name,URL,Comment,InsertUid}:GiftDetail) =>{
     return (
         <Wrapper>
             <span>{Rank === 1 ? '👑' : null}{Rank}位</span>
