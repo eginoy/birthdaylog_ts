19ddfec439679261e7220bd161edc6a8d81657b3
eginoy <eginoy.oheso@gmail.com>
2021-02-09 23:14:33 +0900

    PresentAPIとテストの実装途中

--- src/api/PresentAPI.ts
+++ src/api/PresentAPI.ts
@@ -1,6 +1,10 @@
 import firebase from '../firebase'
+import {Present} from '../types/Present'
 
 const db_ref = process.env.REACT_APP_DB_REF_PRESENTS
 export const GiftDB = firebase.firestore().collection(db_ref as string)
 
 //TODO: Jestでfirebaseのリアルタイムデータベースを操作する関数に対してテスト書く
+export function registPresent(present: Present){
+
+}
